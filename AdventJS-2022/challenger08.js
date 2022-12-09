35e2ea72e66abef4768c1df452f5c4d1ebdde432
Agust1n <agucomba@gmail.com>
2022-12-09 17:19:09 -0300

    reto 08

--- AdventJS-2022/challenger08.js
+++ AdventJS-2022/challenger08.js
@@ -1,16 +1,3 @@
-/* 
-checkPart("uwu") // true
-// "uwu" es un palíndromo sin eliminar ningún carácter
-
-checkPart("miidim") // true
-// "miidim" puede ser un palíndromo después de eliminar la primera "i"
-// ya que "midim" es un palíndromo
-
-checkPart("midu") // false
-// "midu" no puede ser un palíndromo después de eliminar un carácter
-
-*/
-
 function checkPart(part) {
     const reverseString = part.split("").reverse().join("")
     if (part !== reverseString) {
@@ -27,20 +14,3 @@ function checkPart(part) {
     return true
 }
 
-console.log(checkPart('uwu'))
-console.log(checkPart('miidim'))
-console.log(checkPart('agu'))
-
-// let part = "miidim"
-// let reverseString = part.split("").reverse().join("")
-// for (let i = 0; i < part.length; i++) {
-//     let stringArr = reverseString.split("")
-//     stringArr.splice(i, 1)
-//     let arrFinal = stringArr.splice(0, stringArr.length).join("")
-//     if(arrFinal == arrFinal.split("").reverse().join("")){
-//         console.log("true")
-//     }
-//     console.log("----------")
-//     console.log("Array invertido: "+arrFinal.split("").reverse().join(""))
-//     console.log("Array original: "+arrFinal)
-// }
